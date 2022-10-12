24bc08e51b3ad3683816846645cd964d643343e1
Steven-gibson <70613855+Steven-gibson@users.noreply.github.com>
2022-10-12 14:41:11 -0500

    improved version using functions

--- first_version.py
+++ first_version.py
@@ -1,14 +1,18 @@
-for i in range(1,101):
-    if i % 5==0 and i % 3 ==0:
-        print('fizzbuzz')
-        continue
-    elif i % 5 ==0:
-        print('Buzz')
-        continue
-    elif i % 3 == 0:
-        print('fizz')
-        continue
-    else:
-        print(i)
+
+print('Welcome to FizzBuzz')
+def FizzBuzz():
+    for i in range(1,101):
+        if i % 5==0 and i % 3 ==0:
+            print('fizzbuzz')
+            continue
+        elif i % 5 ==0:
+            print('Buzz')
+            continue
+        elif i % 3 == 0:
+            print('fizz')
+            continue
+        else:
+            print(i)
         
     
+FizzBuzz()
